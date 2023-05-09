52a55a005b75b9d17899b52af0fd7400c616ae91
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-05-09 22:18:13 +0530

    express sv

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('TaskManager')
+const express= require('express')
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send("THIS IS HOME")
+})
+
+app.listen(5000, ()=>{
+    console.log("APP ON PORT 5000...")
+})
