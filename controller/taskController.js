571f93f5ea41a91585d74b8bd553db59c1428b92
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-05-31 12:25:06 +0530

    added routes and controller

--- controller/taskController.js
+++ controller/taskController.js
@@ -1,8 +1,24 @@
 function getAllTasks(req,res){
     res.send("contreoller working")
+}
 
+function createTask(req,res){
+    res.send("create tasks")
+}
+function getTask(req,res){
+    res.send("get Single task")
+}
+function updateTask(req,res){
+    res.send("update tasks")
+}
+function deleteTask(req,res){
+    res.send("delete tasks")
 }
 
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
 }
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,7 +1,14 @@
 const express =require('express')
 const route = express.Router()
-const {getAllTasks} = require('../controller/taskController')
+const {
+    getAllTasks,   
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+        } = require('../controller/taskController')
 
-route.route('/').get(getAllTasks)
+route.route('/').get(getAllTasks).post(createTask)
+route.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
 module.exports = route
