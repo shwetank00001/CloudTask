5504757a6f1279d10495612fc1a6b6a7dbdf4f3e
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-06-15 03:32:35 +0530

    always use try catch instead of if else(halts program)

--- controller/taskController.js
+++ controller/taskController.js
@@ -5,8 +5,13 @@ function getAllTasks(req,res){
     res.send("COntroller working")
 }
 async function createTasks(req,res){
-    const task = await Task.create(req.body)  // this will create task in mongoose db using async await
+    try
+    {const task = await Task.create(req.body)  // this will create task in mongoose db using async await
     res.send(task).status(201)
+    }
+    catch(error){
+        res.send({msg:error})
+    }
 }
 function getTask(req,res){
     res.json(req.params)
