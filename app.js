11d1e284d65ab626b373c0d4ee1b81a357aaa743
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2023-06-25 19:28:50 +0530

    added cors to get data in FE

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use(express.static('./public'))
 app.use(express.json());
 app.use(express.urlencoded());
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 // app.use(express.static,('./public'))'
 
 app.get('/hi', function(req,res){
